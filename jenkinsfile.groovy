a5a5c91aa33023a86fab6351162f52f10906b2ca
Tobshapton <tobias.shapton@curiosity.software>
2024-05-30 11:21:14 +0100

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -6,7 +6,9 @@ pipeline {
             steps {
                 echo 'Building...'
                 // Executing an HTTP GET request using curl
-                sh 'curl -X GET "https://partner.testinsights.io/api/apikey/1R0_mB8hMfPNex860y34u1ftR/project/release/published-test-suite/45/execute"'
+                sh '''
+                  curl -X GET "https://partner.testinsights.io/api/apikey/1R0_mB8hMfPNex860y34u1ftR/project/release/published-test-suite/45/execute"
+                '''
                 // Add your build steps here, e.g., compile the code, run build tools
                 // sh 'make' (if you're using make)
                 // sh 'mvn clean install' (if you're using Maven)
